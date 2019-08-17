3faa00ba0cb75374c2286374ce201e681e61ff15
Charlie Sparks <csparks@antioch.edu>
2019-08-17 15:51:01 -0400

    custom event emitting wired up between SearchBar and App

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <SearchBar>
+        <SearchBar v-on:termChange="onTermChange">
         </SearchBar>
     </div>
 </template>
@@ -12,6 +12,11 @@ export default {
     name: 'App',
     components: {
         SearchBar
+    },
+    methods: {
+        onTermChange(searchTerm) {
+            alert(searchTerm);
+        }
     }
 };
 </script>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,19 +1,21 @@
 <template>
     <div>
-        <input @input="onInput" />
-        <p>{{ textInput }}</p>
+        <input @input="onInput" @change="onInputChange"/>
     </div>
 </template>
 
 <script>
 export default {
     name: 'SearchBar',
-    data() {
+    data(){
         return {
             textInput: ''
-        }
+        };
     },
     methods: {
+        onInputChange() {
+            this.$emit('termChange', this.textInput);
+        },
         onInput(event) {
             this.textInput = event.target.value;
         }
