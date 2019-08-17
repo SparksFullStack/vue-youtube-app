7c6b86af240753d0bb08307345182d6b22a53f75
Charlie Sparks <csparks@antioch.edu>
2019-08-17 15:01:26 -0400

    basic project structure setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>howdy</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+
+    },
+    methods: {
+        
+    }
+};
+</script>
